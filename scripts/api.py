00410bd2249e5f6c07daf0f47b2564428efd2dba
Emil Ahlbäck <emil.ahlback99@gmail.com>
2023-04-12 12:37:37 +0200

    use agent_id as namespace

--- scripts/api.py
+++ scripts/api.py
@@ -124,6 +124,7 @@ def simple_api():
 
     # openai.api_key = openai_key
 
+    cfg.pinecone_namespace = request_data["agent_id"]
     memory = get_memory(cfg)
 
     if openai_key != "" and openai_key is not None:
