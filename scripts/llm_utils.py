bcf4d1c6b0f03aba4de8ac1bc7045eb66bc94aa9
Emil Ahlbäck <emil.ahlback99@gmail.com>
2023-04-13 17:02:58 +0200

    remove prompt print

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -1,3 +1,4 @@
+import time
 import openai
 from config import Config
 cfg = Config()
@@ -7,6 +8,7 @@ openai.api_key = cfg.openai_api_key
 # Overly simple abstraction until we create something better
 def create_chat_completion(messages, model=None, temperature=None, max_tokens=None)->str:
     """Create a chat completion using the OpenAI API"""
+    starttime = time.time()
     if cfg.use_azure:
         response = openai.ChatCompletion.create(
             deployment_id=cfg.openai_deployment_id,
@@ -22,5 +24,7 @@ def create_chat_completion(messages, model=None, temperature=None, max_tokens=No
             temperature=temperature,
             max_tokens=max_tokens
         )
+
+    print(f"CHAT COMPLETION TOOK {time.time() - starttime} SECONDS")
 
     return response.choices[0].message["content"]
