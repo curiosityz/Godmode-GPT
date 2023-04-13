1512824262b9f679e91e366febc36a327aae5cc6
Emil Ahlbäck <emil.ahlback99@gmail.com>
2023-04-13 18:05:36 +0200

    remove argparse

--- scripts/main.py
+++ scripts/main.py
@@ -14,7 +14,7 @@ from json_parser import fix_and_parse_json
 from ai_config import AIConfig
 import traceback
 import yaml
-import argparse
+# import argparse
 import logging
 
 cfg = Config()
@@ -291,38 +291,39 @@ def prompt_user():
 
 
 def parse_arguments():
-    """Parses the arguments passed to the script"""
-    global cfg
-    cfg.set_continuous_mode(False)
-    cfg.set_speak_mode(False)
+    return
+    # """Parses the arguments passed to the script"""
+    # global cfg
+    # cfg.set_continuous_mode(False)
+    # cfg.set_speak_mode(False)
 
-    parser = argparse.ArgumentParser(description="Process arguments.")
-    parser.add_argument(
-        "--continuous", action="store_true", help="Enable Continuous Mode"
-    )
-    parser.add_argument("--speak", action="store_true", help="Enable Speak Mode")
-    parser.add_argument("--debug", action="store_true", help="Enable Debug Mode")
-    parser.add_argument(
-        "--gpt3only", action="store_true", help="Enable GPT3.5 Only Mode"
-    )
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description="Process arguments.")
+    # parser.add_argument(
+    #     "--continuous", action="store_true", help="Enable Continuous Mode"
+    # )
+    # parser.add_argument("--speak", action="store_true", help="Enable Speak Mode")
+    # parser.add_argument("--debug", action="store_true", help="Enable Debug Mode")
+    # parser.add_argument(
+    #     "--gpt3only", action="store_true", help="Enable GPT3.5 Only Mode"
+    # )
+    # args = parser.parse_args()
 
-    if args.continuous:
-        print_to_console("Continuous Mode: ", Fore.RED, "ENABLED")
-        print_to_console(
-            "WARNING: ",
-            Fore.RED,
-            "Continuous mode is not recommended. It is potentially dangerous and may cause your AI to run forever or carry out actions you would not usually authorise. Use at your own risk.",
-        )
-        cfg.set_continuous_mode(True)
+    # if args.continuous:
+    #     print_to_console("Continuous Mode: ", Fore.RED, "ENABLED")
+    #     print_to_console(
+    #         "WARNING: ",
+    #         Fore.RED,
+    #         "Continuous mode is not recommended. It is potentially dangerous and may cause your AI to run forever or carry out actions you would not usually authorise. Use at your own risk.",
+    #     )
+    #     cfg.set_continuous_mode(True)
 
-    if args.speak:
-        print_to_console("Speak Mode: ", Fore.GREEN, "ENABLED")
-        cfg.set_speak_mode(True)
+    # if args.speak:
+    #     print_to_console("Speak Mode: ", Fore.GREEN, "ENABLED")
+    #     cfg.set_speak_mode(True)
 
-    if args.gpt3only:
-        print_to_console("GPT3.5 Only Mode: ", Fore.GREEN, "ENABLED")
-        cfg.set_smart_llm_model(cfg.fast_llm_model)
+    # if args.gpt3only:
+    #     print_to_console("GPT3.5 Only Mode: ", Fore.GREEN, "ENABLED")
+    #     cfg.set_smart_llm_model(cfg.fast_llm_model)
 
 
 # TODO: fill in llm values here
