a295eb965ee30e4c2b7773cb4d1f6e8bbd0b1825
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2023-04-15 13:21:36 +0200

    fix

--- scripts/api.py
+++ scripts/api.py
@@ -176,7 +176,7 @@ def get_remote_address() -> str:
     )
 
 
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(app=app, key_func=get_remote_address)
 
 app.wsgi_app = LogRequestDurationMiddleware(app.wsgi_app)
 
